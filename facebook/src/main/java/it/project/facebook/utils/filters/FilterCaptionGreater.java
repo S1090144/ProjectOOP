ce1be60beb238f70da33a98b45d2a1fbfe186f0e
Mattia Scuriatti <S1090144@studenti.univpm.it>
2020-07-14 16:34:41 +0200

    Modificato filtro CaptionGreater

--- facebook/src/main/java/it/project/facebook/utils/filters/FilterCaptionGreater.java
+++ facebook/src/main/java/it/project/facebook/utils/filters/FilterCaptionGreater.java
@@ -6,11 +6,16 @@ public class FilterCaptionGreater extends FilterInt1 implements Filter {
 	public FilterCaptionGreater(Object paramFilter) {
 		super(paramFilter);
 	}
-	
+	int count;
 	public boolean filter(Photo picture) {
 		if( !(picture.getName()==null)){
-			if( picture.getName().length()>paramFilter) {
-				return true;
+			
+				String caption = picture.getName();
+				String trim = caption.trim();
+				
+				count= trim.split("\\w+").length;
+				if(count > paramFilter) {
+		   return true;
 			}
 		}
 		return false;
